66c2bba88872833dd6018915f7661e80ff7a5267
malikkusman <42khan0@gmail.com>
2024-12-06 14:34:06 +0500

    update

--- index.js
+++ index.js
@@ -91,10 +91,10 @@ app.get('/', (req, res) => {
 });
 
 const DB_URL = process.env.DB_URL;
-const PORT = process.env.PORT || 5000;
-mongoose.set("strictQuery", true);
+const PORT = process.env.PORT;
 
-mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+
+mongoose.connect(DB_URL)
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
